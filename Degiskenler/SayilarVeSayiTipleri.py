41d2462b37c03f2f404ab494bcc201581345b87a
ercankirbiyik <rcnkirbiyik@gmail.com>
2023-07-01 20:41:23 +0300

    Sayılar ve sayı tipleri eklendi

---
+++ Degiskenler/SayilarVeSayiTipleri.py
@@ -0,0 +1,17 @@
+
+# SAYILAR
+# Integer(Tam Sayılar)
+# Float(Ondalıklı Sayılar)
+# Complex(Komplex Sayılar - A + Bj)
+
+A = 10
+print(type(A))
+
+B = 99.9
+print(type(B))
+
+C = 45.j
+print(type(C))
+
+
+# MATH Operatörleri
