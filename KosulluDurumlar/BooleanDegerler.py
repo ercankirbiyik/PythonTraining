942a0ca0ac02ad538e076cda7b3ed6d349fe1c72
ercankirbiyik <rcnkirbiyik@gmail.com>
2023-07-02 11:07:30 +0300

    Boolean Değerler eklendi

---
+++ KosulluDurumlar/BooleanDegerler.py
@@ -0,0 +1,23 @@
+# Boolean değerler >> Mantıksal değerler
+    # True ve False olmak üzere iki değeri vardır
+
+
+A = True
+print(type(A))
+
+B = False
+print(type(B))
+
+
+    # FALSE değeri alması
+        # Değişken herhangi bir değer almamışsa
+        # Değişkenin değeri False ise
+        # Değişken değeri 0(sıfır) ise
+        # Değişkenin değeri None ise false değerini alır
+
+
+print(bool(12.3))    # True
+print(bool(0.0))     # False
+print(bool(False))   # False
+print(bool(True))    # True
+print(bool())        # False
