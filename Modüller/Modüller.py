3f35b72f63f7098c74acd68b3b7843a118b0b4db
ercankirbiyik <rcnkirbiyik@gmail.com>
2023-07-31 16:30:04 +0300

    Modül oluşturma mantığı eklendi

--- Modüller/Modüller.py
+++ Modüller/Modüller.py
@@ -25,4 +25,13 @@ import math
 # Burada Math kütüphanesinden sadece factorial fonksiyonunu import ettik ve sadece onu kullanacağımızı belirttik.
 from math import factorial
 
-print(factorial(4))
+print(factorial(6))
+
+
+"""
+    MODÜL OLUŞTURMA
+        * Local cihazımızda boş bir file oluşturup içine .py uzantılı text dosyası oluşturabiliriz ve bu dosyanın içine kendi fonksiyonlarımızı yazabiliriz.
+        * Oluşturacağımız farklı bir .py uzantılı dosyamızda, bir önceki dosyamızı import ederek içindeki bütün fonksiyonları kullanabiliriz.
+        * IDE'mizde proje için oluşturduğumuz bütün dosyalar ve içindeki .py uzantılı dosyalar bizim modüllerimizi oluşturmaktadır.
+        * Bu dosyaları da birbirine import ederek kullanabiliyoruz.
+"""
