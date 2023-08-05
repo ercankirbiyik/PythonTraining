4a8422ad53ede2d5d64f373def1877f518d0aec0
ercankirbiyik <rcnkirbiyik@gmail.com>
2023-08-05 12:53:21 +0300

    Fibonacci Dizisi örneği eklendi

--- Listeler/Liste Üreteçleri.py
+++ Listeler/Liste Üreteçleri.py
@@ -28,3 +28,23 @@ print("****************")
 String = "Python"
 List3 = [i*3 for i in String]   #Her bir Stringin üç(i*3) defa yazılıp liste haline getirilmesini söylüyoruz.
 print(List3)                    #['PPP', 'yyy', 'ttt', 'hhh', 'ooo', 'nnn']
+
+
+print("Fibonacci Dizisi Örneği : ")
+
+"""
+    Fibonacci Dizisi
+        * Dizideki her sayı kendinden önceki iki sayının toplamına eşittir.
+
+"""
+
+Number1 = 1
+Number2 = 3
+
+Fibonacci = []
+
+for value in range(20):
+    Number1, Number2 = Number2, Number1 + Number2   # Burada başta atadığımız değerlere göre Number1 ve Number2 sayılarının toplamını
+                                                    # bir sonraki sayıya atayarak bir döngü oluşturup Fibonacci serisi oluşturduk.
+    Fibonacci.append(Number2)
+print("Döngü ile oluşturduğumuz Fibonacci Dizisi", Fibonacci) #[4, 7, 11, 18, 29, 47, 76, 123, 199, 322, 521, 843, 1364, 2207, 3571, 5778, 9349, 15127, 24476, 39603]
