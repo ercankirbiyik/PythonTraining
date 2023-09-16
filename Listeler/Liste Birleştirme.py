13f3aa01b6f299d52b4ab1530c78d060fbe4aa84
Ercan Kırbıyık <rcnkirbiyik@gmail.com>
2023-09-16 21:00:10 +0300

    Liste Birleştirme eklendi

--- Listeler/Liste Birleştirme.py
+++ Listeler/Liste Birleştirme.py
@@ -3,11 +3,14 @@
 """
 
 Name = ["Robin", "William", "John", "Oliver", "Mark", "Neil", "James", "Michael", "Yoko", "Jane"]
+print(Name)
 
 Surname = ["Hood", "Shakespeare", "Lennon", "Twist", "Twain", "Armstrong", "Dean", "Jackson", "Ono", "Doe"]
+print(Surname)
 
-Famous = list(zip(Name, Surname))
+Famous = list(zip(Name, Surname))  #Liste birleştime işlemini yapan fonksiyonumuz list(zip())
 print(Famous)
 
+# Listeleri birleştirmek için döngü kullanıyoruz
 for name, surname in Famous:
     print(name, surname)
